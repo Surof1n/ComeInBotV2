4a92cc2ecf5115bbc8dbc758375fa45ae01f6095
Brynze Maxim <mbrynze@ozon.ru>
2021-09-16 11:45:58 +0300

    fix: update tokens

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,12 @@ module.exports = {
       script: "./build/index.js",
       env_production: {
         NODE_ENV: "production",
-        TOKEN: "ODg3OTcxMjE2MDA5MzYzNDU2.YUL5ow.H9MvdWBQGL90hQMThLmD3IK7ns8",
+        TOKEN: "ODg3OTcxMjE2MDA5MzYzNDU2.YUL5ow.lELqlWHQJSlBksIHIaUcuLN-sP8",
         PREFIX: ".",
       },
       env_development: {
         NODE_ENV: "development",
-        TOKEN: "Njk5MzMzMzU5OTk4MDc0OTUw.XpS28g.sSZIe0tYBWK5IRctVhI3uVEXB-w",
+        TOKEN: "Njk5MzMzMzU5OTk4MDc0OTUw.XpS28g.rQ87xjP5q5NZ80HfP7f_SIzb4fk",
         PREFIX: ".",
       },
     },
